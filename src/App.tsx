aa8cc6b1d6c7119b4fe2eafa8360c8819d42f718
Andrei7486 <abelij777@gmail.com>
2026-03-08 19:19:19 +0200

    add app version logging

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AppRouter from "./router/AppRouter";
 
 function App() {
+    console.log("App version:", __APP_VERSION__)
     return <AppRouter />;
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/ika-labs/",
   plugins: [react()],
+  define: {
+    __APP_VERSION__: JSON.stringify(Date.now())
+  }
 })
