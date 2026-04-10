6f040ec112012651cc19d0e720cfc75896a60cbe
Andrei Beliaev <andreibeliaev@Andreis-MacBook-Air.local>
2026-04-10 11:49:25 +0300

    Fix JSON vite for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/ika-labs/",
+  // base: "/ika-labs/",
+  base: "/",
   plugins: [react()],
   define: {
     __APP_VERSION__: JSON.stringify(Date.now())
